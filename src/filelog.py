176d08aeb5a5424840bb1e507b8dc0e68645d91e
Naoya Sawada <naoya@tuntunkun.com>
2014-04-19 22:36:01 +0900

    add file log

---
+++ src/filelog.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from sqlalchemy import MetaData
+from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+Base.metadata = MetaData()
+
+class FileLog(Base):
+	__tablename__ = 'terminal'
+
+	id		= Column(Integer, primary_key=True)
+	operation	= Column(Text)
+	path		= Column(Text)
+	date		= Column(DateTime)
+
+	def __init__(self, operation, path):
+		self.operation	= operation
+		self.path	= path
+		self.date	= datetime.now()
+
+	def __str__(self):
+		return "[operation=%s, path=%s, date=%s]" %(self.name, self.path, self.date)
+
+def getMySQLSession(username, hostname, password, database):
+	uri = 'mysql://%s:%s@%s/%s' %(password, username, hostname, database)
+	engine = create_engine(uri)
+	MySQLSession = sessionmaker(bind=engine)
+
+	session = MySQLSession()
+	return session
+
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from errno import EACCES
 from os.path import realpath
