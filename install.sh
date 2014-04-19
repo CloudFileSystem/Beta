7394fbd8503d36376e8e8c0ea134d84c0f558178
Naoya Sawada <naoya@Naoyas-MacBook-Air.local>
2014-04-19 19:16:07 +0900

    add Darwin.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,45 @@ if [ $EUID -eq 0 ]; then
         SUDO=""
 fi
 
-# get linux distribution name
-read dist_hint < /etc/issue
-dist_name=`echo $dist_hint | awk '{print $1}'`
-deploy_script="./dist/${dist_name,,}.sh"
+# OSの判別
+if [ `uname` = "Darwin" ]; then
+	# mac用のコード
+	# ==get mac os name==
+	dist_name=`uname`
+	deploy_script="./dist/${dist_name}.sh"
 
-# ディストリビューションのデプロイスクリプトの存在確認
-if [ -f $deploy_script ]
-then
-	# get source
-	source $deploy_script
-else
-	# error message
-	echo "Deploy script($deploy_script) not found!!"
-	exit
+	echo $dist_name
+
+	# ディストリビューションのデプロイスクリプトの存在確認
+	if [ -f $deploy_script ]
+	then
+		# get source
+		source $deploy_script
+	else
+		# error message
+		echo "Deploy script($deploy_script) not found!!"
+		exit
+	fi
+elif [ `uname` = "Linux" ]; then
+	# =Linux用のコード=
+	# ==get linux distribution name==
+	read dist_hint < /etc/issue
+	dist_name=`echo $dist_hint | awk '{print $1}'`
+	deploy_script="./dist/${dist_name,,}.sh"
+
+	# ディストリビューションのデプロイスクリプトの存在確認
+	if [ -f $deploy_script ]
+	then
+		# get source
+		source $deploy_script
+	else
+		# error message
+		echo "Deploy script($deploy_script) not found!!"
+		exit
+	fi
 fi
 
-
+# mntディレクトリの作成
 if [ -f mnt ]
 then
 	# add mkdir mnt
